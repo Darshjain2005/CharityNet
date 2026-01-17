aeaea28f5b20ea34ccc5c525562d0fa64d8e849b
Darshjain2005 <165250103+Darshjain2005@users.noreply.github.com>
2026-01-17 10:30:40 +0530

    Fix import case for contribute.css

--- src/components/contribute.jsx
+++ src/components/contribute.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Home } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Contribute.css';
+import './contribute.css';
 
 const Contribute = () => {
   const navigate = useNavigate();
